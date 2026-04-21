d887e5a7b0cffb496cd12634953a69df9ade0b9b
hlcxpl <hlcxpl@gmail.com>
2026-04-21 17:24:34 -0400

    feat: integrate swagger documentation with environment toggle

---
+++ src/config/swagger.js
@@ -0,0 +1,23 @@
+const swaggerJsDoc = require('swagger-jsdoc');
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'API de Mantenedor de Boletos y Publicidad',
+            version: '1.0.0',
+            description: 'Documentación de la API para la gestión de Totems, Videos y Empresas.',
+        },
+        servers: [
+            {
+                url: `http://localhost:${process.env.PORT || 3000}`,
+                description: 'Servidor Local',
+            },
+        ],
+    },
+    apis: ['./src/routes/*.js'], // Ruta a los archivos con anotaciones
+};
+
+const specs = swaggerJsDoc(options);
+
+module.exports = specs;
