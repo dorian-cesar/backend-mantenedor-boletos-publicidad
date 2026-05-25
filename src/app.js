b3d2dc5695e7e2e9a901f5ef6d0e65bfc96238ef
Fabian Perez <fabian.perezv21@gmail.com>
2026-05-25 10:45:01 -0400

    fix: configurar CORS explícito para subida directa de videos

--- src/app.js
+++ src/app.js
@@ -13,7 +13,11 @@ app.use(helmet({
     contentSecurityPolicy: false,
     crossOriginEmbedderPolicy: false,
 }));
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-api-key']
+}));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(apiKeyMiddleware);
