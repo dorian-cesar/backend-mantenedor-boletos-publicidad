72b0eff383cecfb95c3ceb45518ca8390c557802
hlcxpl <hlcxpl@gmail.com>
2026-05-26 12:43:47 -0400

    fix: make CORS fully permissive to allow all totem requests

--- src/app.js
+++ src/app.js
@@ -13,11 +13,7 @@ app.use(helmet({
     contentSecurityPolicy: false,
     crossOriginEmbedderPolicy: false,
 }));
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'x-api-key']
-}));
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(apiKeyMiddleware);
